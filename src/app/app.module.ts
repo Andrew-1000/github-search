44e18cd92d7a134d26fecbdb735a738e90347727
Andrew-1000 <andruambia95@gmail.com>
2020-01-18 11:22:02 +0300

    Check for errors that prevents it from showing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -24,6 +25,7 @@ import { DateCountPipe } from './date-count.pipe';
   imports: [
     BrowserModule,
     HttpModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [GithubService],
--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -4,7 +4,8 @@ import { GithubService } from '../github-services/github.service';
 @Component({
   selector: 'app-github',
   templateUrl: './github.component.html',
-  styleUrls: ['./github.component.css']
+  styleUrls: ['./github.component.css'],
+  providers:[GithubService]
 })
 export class GithubComponent implements OnInit {
 
