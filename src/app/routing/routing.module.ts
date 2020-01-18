3b99d90a80b9920fe578eaec3ebd3c0671613ff6
Andrew-1000 <andruambia95@gmail.com>
2020-01-18 18:55:52 +0300

    Remove the contact page from routing module

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { GithubComponent } from '../github/github.component';
 import { AboutComponent } from '../about/about.component';
-import { ContactComponent } from '../contact/contact.component';
+
 import { NotFoundComponent } from '../not-found/not-found.component';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -11,7 +11,6 @@ import { RouterModule, Routes } from '@angular/router';
 const routes:Routes=[
   {path:"", component: GithubComponent},
   {path:"about", component:AboutComponent},
-  {path:"contact", component:ContactComponent},
   {path:"**", component:NotFoundComponent}
 ]
 
